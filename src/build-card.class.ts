4324832e3af891e5aed036fa024a1068c65a57b9
Luis Gutiérrez <luisantonio.gutierrezhernandez@thosmsonreuters.com>
2020-10-05 09:38:54 -0500

    Create BuildCard class

---
+++ src/build-card.class.ts
@@ -0,0 +1,39 @@
+const yaml = require('yaml');
+
+export class BuildCard {
+    private card: any;
+    constructor() {
+        this.card = {};
+        this.card["@type"] = "MessageCard";
+        this.card["@context"] = "https://schema.org/extensions";
+    }
+
+    setTitle(title: string): void {
+        this.card["title"] = title;
+    }
+
+    setSummary(summary: string): void {
+        this.card["summary"] = summary;
+    }
+
+    setThemeColor(themeColor: string): void {
+        this.card["themeColor"] = themeColor;
+    }
+
+    setSections(sections: string): void {
+        const sectionsObject: any = yaml.parse(sections);
+
+        this.card["sections"] = sectionsObject;
+    }
+
+    setPotentialAction(potentialAction: string): void {
+        const potentialActionObject: any = yaml.parse(potentialAction);
+
+        this.card["potentialAction"] = potentialActionObject;
+
+    }
+
+    toObject(): any {
+        return this.card;
+    }
+}
