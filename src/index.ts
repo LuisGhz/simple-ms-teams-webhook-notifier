6055dd4c6daacc84a8f1323cad5ff88fddfce962
Luis Gutiérrez <luisghtz@outlook.com>
2020-10-06 23:14:20 -0500

    Add axios to get avatar_url. Make run async.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import axios from 'axios';
 import { BuildCard } from './class/build-card.class';
 
-function run() {
+async function run() {
+
+  await axios.get(`https://api.github.com/users/${github.context.actor}`)
+  .then((res: any) => {
+      core.info('Setup Ok');
+      process.env.avatar_url = res.data.avatar_url;
+  }).catch((err: any) => {
+      core.error(err);
+  });
+  
   // Get inputs
   const webhook_url = core.getInput('webhook_url', { required: true });
   const summary = core.getInput('summary');
