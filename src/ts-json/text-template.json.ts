cd9ec03d24759cd7ebc51266a4f0e78e94177fe8
Luis Gutiérrez <luisghtz@outlook.com>
2020-10-25 15:19:05 -0600

    Add actor-link text template

--- src/ts-json/text-template.json.ts
+++ src/ts-json/text-template.json.ts
@@ -4,6 +4,7 @@ import { TextTemplate } from '../interface/text-template.interface';
 export const textTemplate: TextTemplate[] = [
     { target: 'actor', replace: github.context.actor },
     { target: 'actor-url', replace: `https://github.com/${ github.context.actor }` },
+    { target: 'actor-link', replace: `[${github.context.actor}](https://github.com/${ github.context.actor })` },
     { target: 'avatar-url', replace: `${process.env.avatar_url}` },
     { target: 'run-number-link', replace: `[#${ github.context.runNumber }](${ github.context.payload?.repository?.html_url }/actions/runs/${ github.context.runId })` }
 ]
