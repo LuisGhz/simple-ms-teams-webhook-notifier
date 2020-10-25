d3900b28bf57aafc4f53ad6ab41cd2150a26e67a
Luis Gutiérrez <luisghtz@outlook.com>
2020-10-25 09:43:22 -0600

    Add Canceled color.

--- src/ts-json/theme-color-template.json.ts
+++ src/ts-json/theme-color-template.json.ts
@@ -2,7 +2,8 @@ const themeColorTemplate: any = {
   Success: '28a745',
   Warning: 'ffc107',
   Error: 'dc3545',
-  Info: '2554fc'
+  Info: '2554fc',
+  Canceled: '4f4f4f'
 }
 
 export default themeColorTemplate;
