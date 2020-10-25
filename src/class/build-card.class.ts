28ee96ee1b750c8d54456fc463654b4e9bd1d4c0
Luis Gutiérrez <luisghtz@outlook.com>
2020-10-25 09:14:52 -0600

    Move text templates (regexReplacer) to separate file

--- src/class/build-card.class.ts
+++ src/class/build-card.class.ts
@@ -2,18 +2,14 @@ import * as core from '@actions/core';
 import *  as github from '@actions/github';
 import YAML from 'yaml'
 
-import { RegexReplacer } from '../interface/RegexReplacer.interface';
+import { textTemplate } from '../ts-json/text-template.json';
 
 export class BuildCard {
     private card: any;
     private preThemeColor: any;
-    private regexReplacer: RegexReplacer[];
     private identNumber: number;
 
     constructor() {
-        const githubContext: any = github.context;
-        const githubActor: string = githubContext.actor;
-
         this.card = {};
         this.card["@type"] = "MessageCard";
         this.card["@context"] = "https://schema.org/extensions";
@@ -26,13 +22,6 @@ export class BuildCard {
             Error: 'dc3545',
             Info: '2554fc'
         }
-
-        this.regexReplacer = [
-            { target: 'actor', replace: githubActor },
-            { target: 'actor-url', replace: `https://github.com/${githubActor}` },
-            { target: 'avatar-url', replace: `${process.env.avatar_url}` },
-            { target: 'run-number-link', replace: `[#${githubContext.runNumber}](${githubContext.payload.repository.html_url}/actions/runs/${githubContext.runId})` }
-        ]
     }
 
     setTitle(title: string): void {
@@ -77,7 +66,7 @@ export class BuildCard {
     }
 
     private replaceTemplates(str: string): string {
-        this.regexReplacer.map(el => {
+        textTemplate.map(el => {
             str = str.replace(new RegExp(`{gh:${el.target}}`, 'g'), el.replace);
         });
 
