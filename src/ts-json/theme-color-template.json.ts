cecbc7ed9210aa2d7a8a8c8e5c6944a0654cd8fd
Luis Gutiérrez <luisghtz@outlook.com>
2020-10-25 12:49:51 -0600

    Export const directly

--- src/ts-json/theme-color-template.json.ts
+++ src/ts-json/theme-color-template.json.ts
@@ -1,9 +1,7 @@
-const themeColorTemplate: any = {
+export const themeColorTemplate: any = {
   Success: '28a745',
   Warning: 'ffc107',
   Error: 'dc3545',
   Info: '2554fc',
   Canceled: '4f4f4f'
 }
-
-export default themeColorTemplate;
